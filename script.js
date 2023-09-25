e231724698fb4206a514cf4a91a4384e51362f60
Vladimir_Chernyaev <>
2023-09-25 15:41:51 +0400

    the patterns have been done

---
+++ script.js
@@ -0,0 +1,11 @@
+// дан объект numbers. Необходимо вывести в консоль все значения больше или равные 3
+
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+}
