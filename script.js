c16ab5d0c158d11420e2729878877f6b88a73621
Vladimir_Chernyaev <>
2023-10-11 02:01:11 +0400

    forth task has been added

--- script.js
+++ script.js
@@ -94,10 +94,51 @@ products.forEach((element) => {
   for (const key in element) {
     if (key == "price") {
       element[key] = element[key] - element[key] * 0.15;
-      }
+    }
   }
   console.log(element);
 });
+
+// Задание 4
+// 1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод filter. Исходные данные - массив products.
+// 2. Необходимо отсортировать массив products используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+
+const products2 = [
+  {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg", "2.jpg"],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg"],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+let productsWithPhotos = products2.filter(function (arr) {
+  for (const key in arr.photos) {
+    if (arr.photos[key].length != 0) {
+      return arr.photos;
+    }
+  }
+});
+console.log(productsWithPhotos);
+
+// 2. Необходимо отсортировать массив products используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+let sortProducts = products2.sort((x,y) => x.price - y.price);
+console.log(sortProducts);
+
+
 
 // **Задание 5**
 // Дано 2 массива
